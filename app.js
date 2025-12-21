0ccf44d2c346d40afcc3268222274127f7cbf4f1
Christopher Thomasson <juliusjoyner87@outlook.com>
2025-12-21 15:59:38 -0800

    Update code initially.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // Import Express.js
 const express = require('express');
+const request = require('requests');
 
 // Create an Express app
 const app = express();
@@ -10,6 +11,8 @@ app.use(express.json());
 // Set port and verify_token
 const port = process.env.PORT || 3000;
 const verifyToken = process.env.VERIFY_TOKEN;
+const accessToken = process.env.ACCESS_TOKEN
+const whatsappBusinessPhoneId = prcess.env.WHATSAPP_BUSINESS_PHONE_NUMBER_ID
 
 // Route for GET requests
 app.get('/', (req, res) => {
@@ -28,6 +31,26 @@ app.post('/', (req, res) => {
   const timestamp = new Date().toISOString().replace('T', ' ').slice(0, 19);
   console.log(`\n\nWebhook received ${timestamp}\n`);
   console.log(JSON.stringify(req.body, null, 2));
+  console.log(req.body.entry.changes.contactswa_id)
+
+
+  const url = `https://graph.facebook.com/v24.0/${whatsappBusinessPhoneId}/messages`
+  const headers = {
+      "Authorization": `Bearer ${accessToken}`,
+      "Content-Type": "application/json",
+  }
+  const data = {
+      "messaging_product": "whatsapp",
+      "to": "919891047436",
+      "text": {
+        "preview_url": false,
+        "body": "This is my response"
+    }
+  }
+  
+  const = constresponse = requests.post(url, headers=headers, json=data, timeout=30)
+
+  
   res.status(200).end();
 });
 
